24830b7193e3766fb6e3b25cfdf334b55d8790b6
NNatha05 <93211379+SkyAnge05@users.noreply.github.com>
2025-03-28 11:36:32 +0100

    création

---
+++ Pointeur1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+void modification (int nommbre);
+
+    int nombre;
+    int *ptr_nombre; // Déclaration d'un pointeur de type entier.
+
+    printf("Entrez un entier : ");
+    scanf("%d", &nombre);
+
+    printf("L'adresse de \"nombre\" est 0x%p.\n",&nombre);
+    printf("La valeur de \"nombre\" est %d.\n", nombre);
+
+    ptr_nombre = &nombre; // ptr_nombre contient l'adresse de nombre.
+
+    *ptr_nombre = 100; // On modifie le contenu de l'adresse mémoire.
+
+    modification(nombre);
+
+    printf("\nAprès la modification :\n");
+
+    printf("L'adresse de \"nombre\" est 0x%p.\n", (void *)&nombre);
+    printf("La valeur de \"nombre\" est %d.\n", nombre);
+    printf("La valeur de \"ptr_nombre\" est 0x%p.\n", (void *)ptr_nombre);
+
+    return 0;
+}
+void modification (int nombre)
+{
+    nombre =+ 10;
+    printf("\n Dans la fonction : nombre vaut %d\n", nombre);
+}
