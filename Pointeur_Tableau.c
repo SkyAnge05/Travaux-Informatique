9dbf2af6a144e4054de50f54cb27bef02e7bc510
NNatha05 <93211379+SkyAnge05@users.noreply.github.com>
2025-04-04 10:27:52 +0200

    Rajoute de la boucle for et du printf

--- Pointeur_Tableau.c
+++ Pointeur_Tableau.c
@@ -3,38 +3,25 @@
 
 int main(){
 
-int T[10] = {-3, 4, 0, -7, 3, 8, 0, -1, 4, -9};
-int POS[10];
-int I, J; /*Indice se courant dans T et POS*/
+    int T[10] = {-3, 4, 0, -7, 3, 8, 0, -1, 4, -9};
+    int POS[10];
+    int I, J; /*Indice se courant dans T et POS*/
 
-for (J=0 , I=0; I<10; I++){
-    if(*(T+I)>0){
-        *(POS+J)=*(T+I);
-        J++;
+    for (J = 0, I = 0; I < 10; I++) {
+        if (*(T + I) > 0) {
+            *(POS + J) = *(T + I);
+            J++;
+        }
     }
-}
-return 0;
-}
 
+    // Affichage des valeurs positives extraites
+    printf("Valeurs positives extraites :\n");
+    for (I = 0; I < J; I++) {
+        printf("%d ", *(POS + I));
+    }
+    printf("\n");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
 
 
-
-
-
-    
