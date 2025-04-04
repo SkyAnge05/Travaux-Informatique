6ba293a94b3d63f8d045a1adc94b8b79224273e6
NNatha05 <93211379+SkyAnge05@users.noreply.github.com>
2025-04-04 12:03:55 +0200

    Rajout

--- Labo_Tableau-Pointeur/Exercice_Tableau-Pointeur.c
+++ Labo_Tableau-Pointeur/Exercice_Tableau-Pointeur.c
@@ -1,17 +1,41 @@
 /*
-Date : 2023-10-05
+Date : 2025-04-04
 Auteur : Nelen Nathan
 */
 #include <stdio.h>
-#include <stdlib.h>
+#define TAILLE 100
 
-int main(){
+void sommeEtProduit(int *tab, int taille, int *somme, int *produit) {
+    *somme = 0;
+    *produit = 1;
+    int i;
+
+    for (i = 0; i < taille; i++) {
+        *somme += tab[i];
+        *produit *= tab[i];
+    }
+}
+
+int main() {
+    int tableau[TAILLE];
+    int somme, produit;
+    int i;
+    int qtt;
+
+    printf("combien d'element voulez vous utiliser : ");
+    scanf("%d", &qtt);
+    printf("Entrez %d entiers :\n", qtt);
+    for (i = 0; i < qtt; i++) {
+        printf("Element %d : ", i + 1);
+        scanf("%d", &tableau[i]);
+    }
 
 
+    sommeEtProduit(tableau, qtt, &somme, &produit);
 
 
+    printf("\nSomme des elements : %d\n", somme);
+    printf("Produit des elements : %d\n", produit);
 
-
-
-    
+    return 0;
 }
