cf947a60cae64b44ed187402ec8b25689c22b968
Callipsow <nathan.nelen@outlook.fr>
2026-03-04 10:46:41 +0100

    ajout de pointeur 2

--- poiteur2-1.c
+++ poiteur2-1.c
@@ -5,7 +5,10 @@ int main() {
 int nombre;
 printf("Entrez un nombre entier : ");
 scanf("%d", &nombre);
-printf("L'adresse de \"nombre\" est 0x%x.\n", &nombre);
+
+printf("L'adresse de \"nombre\" est 0x%x.\n", &nombre);//le caractère d'échappement \ est utilisé pour afficher les guillemets dans la chaîne de caractères.
 printf("la valeur de \"nombre\" est %d.\n", nombre);
+
 return 0;
+
 }
