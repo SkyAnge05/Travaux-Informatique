bca1fefaac69219cef841a1d34a917f05df23d71
Callipsow <nathan.nelen.241194@stu.he-ferrer.eu>
2026-03-11 10:37:35 +0100

    Finitions

--- Tableau2/PoiteurTableau.c
+++ Tableau2/PoiteurTableau.c
@@ -1,9 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+#include <locale.h>
+
 //srand(time(NULL));
 int main()
 {
+    setlocale(LC_ALL, "fr_FR.UTF-8");
 /* Création d'une variable et d'un pointeur */
 int a = 25;
 int *P;
@@ -14,18 +17,16 @@ printf("Affichage de la valeur de a : ");
 printf("%d", a);
 
 /* Affichage de l'adresse de a */
-printf("\n Affichage de l'adresse de a : ");
+printf("\n\n Affichage de l'adresse de a : ");
 printf("\n 0x%x", &a);
 
 /*  Affichage de la valeur de p*/
-printf("Affichage de la valeur de p : ");
-printf("\n %d", P);
+printf("\n\nAffichage de la valeur de p : ");
+printf("\n 0x%x", P);
 
 /* Affichage de la valeur pointée par p */
-printf("\n Affichage de la valeur pointée par p : ");
+printf("\n\nAffichage de la valeur pointée par p : ");
 printf("\n %d", *P);
-
-
 
     return 0;
 }
