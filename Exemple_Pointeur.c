10235fd57957e5ae659234aa5afea8e4b986b50a
Callipsow <nathan.nelen@outlook.fr>
2026-03-25 11:56:00 +0100

    ajout de prog exemple

--- Exemple_Pointeur.c
+++ Exemple_Pointeur.c
@@ -1,4 +1,4 @@
-/*comment passer d'un tableau à une fonction*/
+/* Comment passer d'un tableau à une fonction. */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,8 +10,33 @@ int largest(int x[], int y);
 
 int main()
 {
-
-
-
+    /* Lecture des MAX valeurs à partir du clavier. */
     
+    for (count = 0; count < MAX; count++)
+    {
+        printf("Entrez une valeur entière : ");
+        scanf("%d", &tab[count]);
+    }
+    
+    /* Appel de la fonction et affichage de la valeur renvoyée. */
+    
+    printf("\n\nLa valeur la plus grande est %d\n", 
+            largest(tab, MAX));
+    exit(EXIT_SUCCESS);
+}
+
+/* La fonction largest() renvoie la valeur la plus grande */
+/* d'un tableau d'entiers. */
+
+int largest(int x[], int y)
+{
+    int count, biggest = x[0];
+    
+    for (count = 1; count < y; count++)
+    {
+        if (x[count] > biggest)
+            biggest = x[count];
+    }
+    
+    return biggest;
 }
