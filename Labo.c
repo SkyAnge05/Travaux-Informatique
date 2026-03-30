9ad742e2d7fc08a590c9d0bbea171f5e0ba1f28e
Callipsow <93211379+SkyAnge05@users.noreply.github.com>
2026-03-30 11:54:36 +0200

    ajout labo

---
+++ Labo.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_CHOIX 10
+
+int main()
+{
+    int choix = MAXCHOIX;
+
+    /* initialisation du menu principale */
+    printf("~~~~~ MENU PRINCIPAL ~~~~~\n");
+    printf("1. Nouveau diagnostique\n");
+    printf("2. Voir Historiqe\n");
+    printf("3. Quitter\n");
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+    scanf("\n%d",&choix);
+
+    switch (choix)
+    {
+       case 1:
+
+
+            break;
+
+            
+       case 2 :
+
+           break;
+           
+           
+       case 3 :
+           
+           
+            break;
+
+    }
+ 
+
+
+
+}
