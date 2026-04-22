ab712178af3a9a5c966de919194a179b0a049719
SkyAnge05 <nathan.nelen.241194@stu.he-ferrer.eu>
2026-04-22 12:03:30 +0200

    exos

--- Exercice9.2.c
+++ Exercice9.2.c
@@ -7,22 +7,39 @@ Un grossiste en composants informatiques vends  quatres types de produits
 Chaques produit possède une références (ce qui est un nombre entier), un prix en euros
 et des quantités disponibles
 */
+
+/*Déclaration d'une structure stokante */
 struct produit
 {
- int carte_mere;/*code 1*/
- int processeur;/*code 2*/
- int RAM;/*code 3*/
- int carte_graphique;/*code 4*/
-}p;
- /* p pour produit */
+ int ref;
+ char produits[20];
+ float prix;
+ int quantités;
+};
+//struct produit p1;
+
+struct produit produit_saisie();
 
 
-int main()
-{
+int main() {
 set_Locale(LC_ALL, "fr_FR");
 
 
+struct produit p;
+p = produit_saisie();
 
+
+
+
+}
+
+struct produit produit_saisie()
+{
+printf("Bonjour et bienvenue le programme de gestion de stock de composants informatique \n");
+
+struct produit p1;
+printf("Veuillez rentre la référence de produit que vous souhaitez ajouter au panier parmis les choix: ");
+printf("\n 1: Processeur");
 
 
 }
