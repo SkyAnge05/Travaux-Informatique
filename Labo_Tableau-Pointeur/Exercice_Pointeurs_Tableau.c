eae35365907fbc02dd8e2cbe9714f5fff8d204ea
Callipsow <93211379+SkyAnge05@users.noreply.github.com>
2026-06-14 18:47:55 +0200

    re

---
+++ Labo_Tableau-Pointeur/Exercice_Pointeurs_Tableau.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define TAILLE 10
+
+void AfficheTableau(int *debut, int *fin) {
+    int *p;
+
+    for (p = debut; p <= fin; p++) {
+        printf("%d ", *p);
+    }
+    printf("\n");
+}
+
+double moyenne(int *debut, int *fin) {
+    int *p;
+    double somme = 0.0;
+    double moyenneCalcul;
+    int nbElements;
+
+    nbElements = fin - debut + 1;
+
+    for (p = debut; p <= fin; p++) {
+        somme += *p;
+    }
+
+    moyenneCalcul = somme / nbElements;
+
+    for (p = debut; p <= fin; p++) {
+        *p = *p * moyenneCalcul;
+    }
+
+    return moyenneCalcul;
+}
+
+int main(void) {
+    int tableau[TAILLE];
+    int *p;
+    double moyenneRetournee;
+
+    srand((unsigned int)time(NULL));
+
+    for (p = tableau; p < tableau + TAILLE; p++) {
+        *p = rand() % 100;
+    }
+
+    printf("Tableau avant appel de moyenne :\n");
+    AfficheTableau(tableau, tableau + TAILLE - 1);
+
+    moyenneRetournee = moyenne(tableau, tableau + TAILLE - 1);
+
+    printf("\nTableau apres modification :\n");
+    AfficheTableau(tableau, tableau + TAILLE - 1);
+
+    printf("\nMoyenne retourne : %.2f\n", moyenneRetournee);
+
+    return 0;
+}
