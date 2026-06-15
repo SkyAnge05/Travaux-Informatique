7dfed2c02477d46092cb08d19afd7fe870a0804b
Callipsow <93211379+SkyAnge05@users.noreply.github.com>
2026-06-15 21:42:55 +0200

    exos 4 et prepa exams

--- Tableau/2-Tableau/Exo4.c
+++ Tableau/2-Tableau/Exo4.c
@@ -2,16 +2,84 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main()
+
+//Définition 
+#define MAX_ENTIER 10
+
+
+//Prototypage des fonction
+
+void moyenne(int *deb, int *fin);
+
+int main(){
+srand(time(NULL));
+int tableau[MAX_ENTIER];
+int i;
+int choix;
+
+
+
+int *deb = &tableau[0];
+/*2
+deb est une adresse
+*deb est la valeur contenue à cette adresse
+*/
+
+int *fin = &tableau[9];
+ /*Remplisage du tableau*/
+ for(i=0; i< MAX_ENTIER; i++)
+ {
+    tableau[i] = 1 + rand() % 20;
+
+ }
+
+
+/*---------------------Teste-------------------------*/
+printf("Le début du tableau : %d", tableau[0]);
+printf("\nLa fin du tableau : %d", tableau[9]);
+/*---------------------Teste-------------------------*/
+
+
+printf("\nrentre ton choix : ");
+scanf("%d", &choix);
+
+switch (choix)
 {
-srand(time(NULL)); // !! Initialiser le générateur de nombres aléatoires !!
+case 1 :
+moyenne(deb, fin);
+break;
+
+case 2 :
+for(i=0; i< MAX_ENTIER; i++)
+ {
+tableau[i] = 1+ rand() %20 ;
+
+ }
+
+break;
+
+
+default:
+
+return EXIT_SUCCESS;
+break;
+
+}
+
+return 0;
+
+}
 
 
 
+/*2
+deb est une adresse
+*deb est la valeur contenue à cette adresse
+*/
 
-
-
-
-
-    return 0;
+⁼ 
+void moyenne(int *deb, int *fin)
+{
+    printf("%d\n", *deb);   // valeur du premier élément
+    printf("%d\n", *fin);   // valeur du dernier élément
 }
--- Teste 2/pointeur.c
+++ Teste 2/pointeur.c
@@ -2,12 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+//Définition 
+#define MAX_ENTIER 10
+
+
 //Prototypage des fonction
 
 void moyenne();
 
 int main(){
 srand(time(NULL));
+ int tableau[MAX_ENTIER];
+int i;
+
+
+ /*Remplisage du tableau*/
+ for(i=0; i< MAX_ENTIER; i++)
+ {
+rand() 1%20 = tableau[i];
+
+ }
 
 
 
